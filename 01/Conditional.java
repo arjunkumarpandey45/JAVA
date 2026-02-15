03714e6f78752d4e797962eb91f0c0cc746c934b
Arjun Pandey <arjun7236201798@gmail.com>
2026-02-15 22:43:22 +0530

    conditionals

---
+++ 01/Conditional.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if (age < 0) {
+            System.out.println("Invalid age");
+        } else if (age < 13) {
+            System.out.println("You are a child");
+        } else if (age < 18) {
+            System.out.println("You are a teenager");
+        } else if (age < 60) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are a senior citizen");
+        }
+
+        sc.close();
+    }
+}
